e6b3019976e972055f41ee186dce7d4bb08ab994
Nibi <75438082+nessnetworkyt72728@users.noreply.github.com>
2022-02-19 12:09:05 +0100

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -17,9 +17,11 @@ using StringTools;
 class CoolUtil
 {
 	public static var defaultDifficulties:Array<String> = [
+		'Noob',
 		'Easy',
 		'Normal',
-		'Hard'
+		'Hard',
+		'Crazy'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
